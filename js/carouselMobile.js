c79bc892d6b33ed67ebffe91c3b050885ac0fad8
Eduardo Sá <111080797+EduardoSa23@users.noreply.github.com>
2025-09-30 08:56:01 -0300

    Update carouselMobile.js

--- js/carouselMobile.js
+++ js/carouselMobile.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
     if (index < 0) index = 0;
     if (index > maxIndex) index = maxIndex;
 
-    track.style.transform = `translateX(-${index * 99}%)`;
+    track.style.transform = `translateX(-${index * 97}%)`;
 
     dots.forEach((dot, i) => {
       dot.classList.toggle("bg-[#EF7D00]", i === index);
